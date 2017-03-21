aa4c1ea6a8b5a2b68fd40f04ba0d8105ac173ef5
DJ Meyers <mrdjmeyers@ufl.edu>
2017-03-21 02:37:21 -0400

    Made a bunch of changes to style and backend.  Not as bad as anticipated.  Made Landing page with basic Create/Join Room buttons.  Text is large for readability on mobile.

---
+++ config.js
@@ -0,0 +1,14 @@
+/*
+ * Config.js
+ *
+ * Contains all of the configurable fields used by this project
+ */
+var path = require('path');
+
+var localhost = '127.0.0.1', ip = '192.168.1.3';
+module.exports = {
+
+	HOST: ip,
+	PORT: '3000',
+
+};
---
+++ server.js
@@ -0,0 +1,70 @@
+/*
+ * Server.js
+ *
+ * The main portion of this project. Contains all the defined routes for express,
+ * rules for the websockets, and rules for the MQTT broker.
+ *
+ * Refer to the portions surrounded by --- for points of interest
+ */
+var express   = require('express'),
+	app       = express();
+var pug       = require('pug');
+var sockets   = require('socket.io');
+var path      = require('path');
+var conf      = require(path.join(__dirname, 'config'));
+
+setupExpress();
+setupSocket();
+
+// Helper functions
+function setupExpress() {
+	app.set('view engine', 'pug'); // Set express to use pug for rendering HTML
+
+	// Setup the 'public' folder to be statically accessable
+	var publicDir = path.join(__dirname, 'public');
+	app.use(express.static(publicDir));
+
+	// Setup the paths (Insert any other needed paths here)
+	// ------------------------------------------------------------------------
+	// Home page
+	app.get('/', (req, res) => {
+		res.render('index', {title: 'Mob Justice'});
+	});
+
+
+	// Basic 404 Page
+	app.use((req, res, next) => {
+		var err = {
+			stack: {},
+			status: 404,
+			message: "Error 404: Page Not Found '" + req.path + "'"
+		};
+
+		// Pass the error to the error handler below
+		next(err);
+	});
+
+	// Error handler
+	app.use((err, req, res, next) => {
+		console.log("Error found: ", err);
+		res.status(err.status || 500);
+
+		res.render('error', {title: 'Error', error: err.message});
+	});
+	// ------------------------------------------------------------------------
+
+	// Handle killing the server
+	process.on('SIGINT', () => {
+		internals.stop();
+		process.kill(process.pid);
+	});
+}
+
+function setupSocket() {
+	var server = require('http').createServer(app);
+	var io = sockets(server);
+
+	server.listen(conf.PORT, conf.HOST, () => {
+		console.log("Listening on: " + conf.HOST + ":" + conf.PORT);
+	});
+}
