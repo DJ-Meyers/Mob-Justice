f8c79a1cc6d808da340859c068d86050b4fc8665
DJ Meyers <mrdjmeyers@ufl.edu>
2017-04-12 03:27:17 -0400

    Cleaned up lots of useless code in server.js.  Began writing BeginDay().

--- server.js
+++ server.js
@@ -97,17 +97,7 @@ io.on('connection', function(socket) {
 				id: socket.id
 			}
 		hashstuff.push(socketId);
-		/*
-			Below is the old logic that was used when everything used join.  Use simple logic like
-			checking if the roomCode is in activeRooms[].  If it is, do socket.join(roomCode), and
-			add the user onto the list of users for that room.  Otherwise, console.log that
-			<username> tried to access a room that doesn't exist.  This way, users won't
-			accidentally create new rooms when entering the wrong room code.
-		*/
 
-		// socket.join(roomCode);
-		//lets you know whose joining
-		// console.log(username + ' has tried to connect to room ' + roomCode);
 		var room = findRoom(roomCode);
 	    if(!room) {
 			console.log("Room " + roomCode + " doesn't exist.");
@@ -119,26 +109,6 @@ io.on('connection', function(socket) {
 				io.to(roomCode).emit('newUser', username, host);
 				console.log(username + ' has connected to ' + roomCode);
 
-			// //Add the room to activeRooms
-			 //console.log(user);
-			 //var users = [user];
-			// if(!room) {
-			// 	room = {
-			// 		roomCode: roomCode,
-			// 		users: users,
-			// 		started: false
-			// 	};
-			// 	activeRooms.push(room);
-			// 	io.to(roomCode).emit('newUser', username);
-			// } else {
-			// 	if(!room.started) {
-			// 		room.users.push(user);
-			// 		io.to(roomCode).emit('newUser', username);
-			// 	} else {
-			// 		console.log( username + ' is attempting to join has already begun.');
-			// 	}
-			// }
-			// console.log(activeRooms);
 		}}
 	});
 	socket.on('getUsers', function(roomCode, username) {
@@ -198,6 +168,7 @@ io.on('connection', function(socket) {
 		room.started = true;
 		assignRoles(room);
 		io.to(roomCode).emit('gameStarted');
+		console.log('Started ame ' + roomCode + " successfully");
 	});
 });
 
@@ -207,38 +178,6 @@ io.on('connection', function(socket) {
 app.get('/', (req, res) => {
 	res.render('index', {title: 'Mob Justice'});
 });
-
-app.get('/currentUsers/:roomCode', (req, res) => {
-	console.log("recieved a request with room code"+ req.params.roomCode);
-	var room = findRoom(req.params.roomCode);
-	console.log(room.users);
-	var names = [];
-	for(var u = 0; u < room.users.length; u++) {
-		var thisName = room.users[u].name;
-		//console.log(thisName);
-		names.push(thisName);
-		console.log(thisName);
-	}
-	// console.log(names);
-	res.send(JSON.stringify(names));
-});
-
-app.post('/startGame/:roomCode', (req, res) => {
-	var room = findRoom(req.params.roomCode);
-	room.started = true;
-	console.log('Starting Game: ' + req.params.roomCode);
-	io.to(req.params.roomCode).emit('gameStarted');
-})
-
-function createRoomCode() {
-	var code = "";
-	for(var i = 0; i < 4; i++) {
-		var letter = String.fromCharCode(Math.random() * (26) + 65);
-		code += letter;
-	}
-	console.log(code);
-	return code;
-}
 
 function findRoom(code) {
 	for(var i = 0; i < activeRooms.length; i++) {
@@ -316,85 +255,6 @@ function assignRoles(room) {
 }
 
 
-//Not needed
-
-
-app.get('/join', (req, res) => {
-	res.render('join', {title: 'Mob Justice - Join'});
-});
-
-app.get('/play', (req, res) => {
-	res.render('play', {title: 'Mob Justice - Play'});
-});
-
-app.get('/create', (req, res) => {
-	res.render('create', {title: 'Mob Justice - Create'});
-});
-
-app.get('/about', (req, res) => {
-	res.render('about', {title: 'Mob Justice - About'});
-});
-
-app.get('/creators', (req, res) => {
-	res.render('creators', {title: 'Mob Justice - Creators'});
-});
-
-
-// Post Request to create new room
-app.post('/newGame', urlencodedParser, (req, res) => {
-	//Initialize doctor and detective to off
-	var doctor = 'off', detective = 'off';
-	var users = [];
-
-	//If they're included in the form submission, set them to on.
-	if(req.body.doctor) {
-		doctor = 'on';
-	}
-	if(req.body.detective) {
-		detective = 'on';
-	}
-
-	var user = {
-		name:req.body.name,
-		role:"citizen"
-	};
-
-	//users.push(user);
-
-	var newGame = {
-		roomCode:createRoomCode(),
-		doctor:doctor,
-		detective:detective,
-		users: [user]
-	};
-
-	activeGames.push(newGame);
-	console.log(user.name + " created: " + newGame);
-	console.log(activeGames);
-
-
-	// Connect to the room.
-
-res.end(JSON.stringify(newGame));
-});
-
-// Post Request to create new room
-app.post('/joinGame', urlencodedParser, (req, res) => {
-	//Find room with given code
-	var room = findRoom(req.body.roomCode);
-
-	//Add user to that room
-	var user = {
-		name:req.body.name,
-		role:"citizen"
-	};
-
-	room.users.push(user);
-	console.log(room);
-
-	res.end(JSON.stringify(user));
-});
-
 // Basic 404 Page
 app.use((req, res, next) => {
 	var err = {
@@ -421,14 +281,3 @@ process.on('SIGINT', () => {
 	internals.stop();
 	process.kill(process.pid);
 });
-
-
-function setupSocket() {
-	var server = require('http').createServer(app);
-	var io = socket(server);
-
-	server.listen(conf.PORT, conf.HOST, () => {
-		console.log("Server listening on: " + conf.HOST + ":" + conf.PORT);
-	});
-
-}
